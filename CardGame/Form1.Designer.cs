f5c4017633bcfeddd01e73ec15087bebe4f89f27
William Burchett <burchettw3@gmail.com>
2018-08-16 12:30:27 -0400

    First commit

---
+++ CardGame/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace CardGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.CloseButton = new System.Windows.Forms.Button();
+            this.NewGameButton = new System.Windows.Forms.Button();
+            this.DrawButton = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.ResultTextBox = new System.Windows.Forms.TextBox();
+            this.PlayerScoreTextBox = new System.Windows.Forms.TextBox();
+            this.ComputerScoreTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.PlayerPictureBox = new System.Windows.Forms.PictureBox();
+            this.ComputerPictureBox = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PlayerPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ComputerPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Firebrick;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.CloseButton);
+            this.panel1.Controls.Add(this.NewGameButton);
+            this.panel1.Controls.Add(this.DrawButton);
+            this.panel1.Location = new System.Drawing.Point(27, 495);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1094, 100);
+            this.panel1.TabIndex = 12;
+            // 
+            // CloseButton
+            // 
+            this.CloseButton.Location = new System.Drawing.Point(945, 37);
+            this.CloseButton.Name = "CloseButton";
+            this.CloseButton.Size = new System.Drawing.Size(132, 47);
+            this.CloseButton.TabIndex = 9;
+            this.CloseButton.Text = "Close";
+            this.CloseButton.UseVisualStyleBackColor = true;
+            this.CloseButton.Click += new System.EventHandler(this.CloseButton_Click);
+            // 
+            // NewGameButton
+            // 
+            this.NewGameButton.Location = new System.Drawing.Point(756, 37);
+            this.NewGameButton.Name = "NewGameButton";
+            this.NewGameButton.Size = new System.Drawing.Size(132, 47);
+            this.NewGameButton.TabIndex = 8;
+            this.NewGameButton.Text = "New Game";
+            this.NewGameButton.UseVisualStyleBackColor = true;
+            this.NewGameButton.Click += new System.EventHandler(this.NewGameButton_Click);
+            // 
+            // DrawButton
+            // 
+            this.DrawButton.Enabled = false;
+            this.DrawButton.Location = new System.Drawing.Point(40, 37);
+            this.DrawButton.Name = "DrawButton";
+            this.DrawButton.Size = new System.Drawing.Size(132, 47);
+            this.DrawButton.TabIndex = 7;
+            this.DrawButton.Text = "Draw";
+            this.DrawButton.UseVisualStyleBackColor = true;
+            this.DrawButton.Click += new System.EventHandler(this.DrawButton_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(905, 29);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(163, 38);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "PLAYER:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Firebrick;
+            this.label3.Location = new System.Drawing.Point(60, 29);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(224, 38);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "COMPUTER:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.ResultTextBox);
+            this.groupBox1.Controls.Add(this.PlayerScoreTextBox);
+            this.groupBox1.Controls.Add(this.ComputerScoreTextBox);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(361, 135);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(431, 289);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Current Scores";
+            // 
+            // ResultTextBox
+            // 
+            this.ResultTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ResultTextBox.Location = new System.Drawing.Point(58, 235);
+            this.ResultTextBox.Name = "ResultTextBox";
+            this.ResultTextBox.ReadOnly = true;
+            this.ResultTextBox.Size = new System.Drawing.Size(302, 45);
+            this.ResultTextBox.TabIndex = 4;
+            this.ResultTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // PlayerScoreTextBox
+            // 
+            this.PlayerScoreTextBox.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.PlayerScoreTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PlayerScoreTextBox.Location = new System.Drawing.Point(242, 158);
+            this.PlayerScoreTextBox.Name = "PlayerScoreTextBox";
+            this.PlayerScoreTextBox.ReadOnly = true;
+            this.PlayerScoreTextBox.Size = new System.Drawing.Size(100, 45);
+            this.PlayerScoreTextBox.TabIndex = 3;
+            this.PlayerScoreTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // ComputerScoreTextBox
+            // 
+            this.ComputerScoreTextBox.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ComputerScoreTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ComputerScoreTextBox.Location = new System.Drawing.Point(242, 34);
+            this.ComputerScoreTextBox.Name = "ComputerScoreTextBox";
+            this.ComputerScoreTextBox.ReadOnly = true;
+            this.ComputerScoreTextBox.Size = new System.Drawing.Size(100, 45);
+            this.ComputerScoreTextBox.TabIndex = 2;
+            this.ComputerScoreTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(76, 179);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Player Score:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(55, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Computer Score:";
+            // 
+            // PlayerPictureBox
+            // 
+            this.PlayerPictureBox.Location = new System.Drawing.Point(836, 89);
+            this.PlayerPictureBox.Name = "PlayerPictureBox";
+            this.PlayerPictureBox.Size = new System.Drawing.Size(285, 400);
+            this.PlayerPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PlayerPictureBox.TabIndex = 8;
+            this.PlayerPictureBox.TabStop = false;
+            // 
+            // ComputerPictureBox
+            // 
+            this.ComputerPictureBox.Location = new System.Drawing.Point(27, 89);
+            this.ComputerPictureBox.Name = "ComputerPictureBox";
+            this.ComputerPictureBox.Size = new System.Drawing.Size(285, 400);
+            this.ComputerPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ComputerPictureBox.TabIndex = 7;
+            this.ComputerPictureBox.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1148, 625);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.PlayerPictureBox);
+            this.Controls.Add(this.ComputerPictureBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PlayerPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ComputerPictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button CloseButton;
+        private System.Windows.Forms.Button NewGameButton;
+        private System.Windows.Forms.Button DrawButton;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox ResultTextBox;
+        private System.Windows.Forms.TextBox PlayerScoreTextBox;
+        private System.Windows.Forms.TextBox ComputerScoreTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox PlayerPictureBox;
+        private System.Windows.Forms.PictureBox ComputerPictureBox;
+    }
+}
+
---
+++ CardGame/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CardGame
+{
+    public partial class Form1 : Form
+    {
+
+        Image[] images = new Image[52];
+        int[] cardValues = new int[52];
+        int currentCard = 0;
+        int computerScore;
+        int playerScore;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int[] values = { 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 };
+            string[] ranks = { "2", "3", "4", "5", "6", "7", "8", "9", "10",
+                "jack", "queen", "king", "ace" };
+            string[] suits = { "diamonds", "hearts", "spades", "clubs" };
+
+            string path = @"C:\temp\cardimages\";
+
+            int currentIndex = 0;
+
+            for (int i = 0; i < ranks.Length; i++)
+            {
+                for (int j = 0; j < suits.Length; j++)
+                {
+                    string filename = $"{path}{ranks[i]}_of_{suits[j]}.png";
+
+                    images[currentIndex] = Image.FromFile(filename);
+                    cardValues[currentIndex] = values[i];
+                    currentIndex++;
+                }
+            }
+        }
+
+        private void NewGameButton_Click(object sender, EventArgs e)
+        {
+            currentCard = 0;
+
+            Random random = new Random();
+            for (int i = 0; i < cardValues.Length - 1; i++)
+            {
+                int r1 = random.Next(i, cardValues.Length);
+                Image image = images[i];
+                int value = cardValues[i];
+
+                images[i] = images[r1];
+                cardValues[i] = cardValues[r1];
+
+                images[r1] = image;
+                cardValues[r1] = value;
+            }
+
+            DrawButton.Enabled = true;
+            NewGameButton.Enabled = false;
+
+            computerScore = 0;
+            playerScore = 0;
+            ComputerScoreTextBox.Text = computerScore.ToString();
+            PlayerScoreTextBox.Text = playerScore.ToString();
+            ResultTextBox.Text = "Ready... Play!";
+            ComputerPictureBox.Image = null;
+            PlayerPictureBox.Image = null;
+        }
+
+        private void DrawButton_Click(object sender, EventArgs e)
+        {
+            ComputerPictureBox.Image = images[currentCard];
+            int computerCardValue = cardValues[currentCard];
+            currentCard++;
+
+            PlayerPictureBox.Image = images[currentCard];
+            int playerCardValue = cardValues[currentCard];
+            currentCard++;
+
+            if (computerCardValue > playerCardValue)
+            {
+                ResultTextBox.Text = "Computer Wins";
+                computerScore = computerScore + 1;
+            }
+            if (computerCardValue < playerCardValue)
+            {
+                ResultTextBox.Text = "Player Wins";
+                playerScore = playerScore + 1;
+            }
+            if (computerCardValue == playerCardValue)
+            {
+                ResultTextBox.Text = "Tie";
+            }
+
+            ComputerScoreTextBox.Text = computerScore.ToString();
+            PlayerScoreTextBox.Text = playerScore.ToString();
+
+
+            if (currentCard == 52)
+            {
+                MessageBox.Show("The entire deck has been dealt!", "Game Over");
+                NewGameButton.Enabled = true;
+                DrawButton.Enabled = false;
+            }
+        }
+        
+        private void CloseButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
